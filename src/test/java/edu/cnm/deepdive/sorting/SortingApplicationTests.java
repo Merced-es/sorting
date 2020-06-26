5e4ec7b082ac9504186a82c58dddbe1fcb10a1f3
Mercedes <mercedes.nmx@gmail.com>
2020-06-26 12:20:01 -0600

    Initial commit

---
+++ src/test/java/edu/cnm/deepdive/sorting/SortingApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cnm.deepdive.sorting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SortingApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
